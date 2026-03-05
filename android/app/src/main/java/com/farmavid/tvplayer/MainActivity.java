c343d10b5680442393fe36674760a7bd5a4984f8
unknown <auto@bot.com>
2026-03-05 14:00:51 -0400

    fix-keep-screen-on

--- android/app/src/main/java/com/farmavid/tvplayer/MainActivity.java
+++ android/app/src/main/java/com/farmavid/tvplayer/MainActivity.java
@@ -1,5 +1,14 @@
 package com.farmavid.tvplayer;
 
+import android.os.Bundle;
+import android.view.WindowManager;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Mantener la pantalla encendida siempre
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    }
+}
